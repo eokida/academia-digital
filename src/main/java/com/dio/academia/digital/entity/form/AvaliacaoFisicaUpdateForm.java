38d387c5696cb8e61830d7c1d6e7d7ec59bd90f3
Eric Okida <ericyujijpn@yahoo.com.br>
2022-05-18 20:28:22 -0300

    First commit

---
+++ src/main/java/com/dio/academia/digital/entity/form/AvaliacaoFisicaUpdateForm.java
@@ -0,0 +1,8 @@
+package com.dio.academia.digital.entity.form;
+
+public class AvaliacaoFisicaUpdateForm {
+
+  private double peso;
+
+  private double altura;
+}
---
+++ src/main/java/com/dio/academia/digital/repository/AvaliacaoFisicaRepository.java
@@ -0,0 +1,10 @@
+package com.dio.academia.digital.repository;
+
+import com.dio.academia.digital.entity.AvaliacaoFisica;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AvaliacaoFisicaRepository extends JpaRepository<AvaliacaoFisica, Long> {
+
+}
